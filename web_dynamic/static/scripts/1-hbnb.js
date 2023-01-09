b0a6c522531322bde0719db062b932f33cf23952
zinnyzee <ndukaabigail1@gmail.com>
2023-01-09 07:56:57 -0800

    airv4

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  const amenity_check = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      amenity_check[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenity_check[$(this).data('id')];
+    }
+    const amenity_list = Object.values(amenity_check);
+    if (amenity_list.length > 0) {
+      $('div.amenities > h4').text(amenity_list.join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+}
+
+);
